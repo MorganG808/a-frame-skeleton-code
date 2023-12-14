d59a0c196884efd3983daa1680bd72f6628435cf
Morgan G <biologyislife@50-50.com>
2023-12-14 05:27:08 +0000

    commit js

--- script.js
+++ script.js
@@ -4,30 +4,16 @@ let xPos = 4;
 let yPos = 0;
 
 function moveWhale(){
-    if (xPos > 4){
-        xPos += -0.03
+    if (xPos < 5 && xPos > -5){
+        xPos += -0.007
     }
     
     else {
         xPos = 4
     }
 
-    whale.setAttribute('position', `${xPos} 3 -5`)
+    whale.setAttribute('position', `${xPos} 0 -4`)
     requestAnimationFrame(moveWhale)
 }
 
 moveWhale()
-
-function switchScene(){
-    const scene1 = document.getElementById("scene1")
-    const scene2 = document.getElementById("scene2")
-
-    if (scene1.style.display !== 'none'){
-       scene1.style.display = 'none';
-       scene2.style.display = 'block';
-    }
-    else{
-        scene1.style.display = 'block';
-        scene2.style.display = 'none'; 
-    }
-}
